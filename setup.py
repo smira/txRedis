4c76808b3c5c1c3ef9b1e801d55cc4edf2351052
Vasiliy Evseenko <vasiliy.evseenko@skype.net>
2013-01-18 10:56:05 +0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 sdict = {
     'name' : 'txredis',
-    'version' : '2.2.qik4',
+    'version' : '2.2.qik5',
 	'packages' : ['txredis'],
     'description' : 'Python/Twisted client for Redis key-value store',
     'author' : 'Dorian Raymer',
